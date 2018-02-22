e049211d4f76327f77ec0a3cc17871661debbdcf
bimiriya <poyarzunaspee@gmail.com>
2018-02-21 23:35:06 -0300

    fetch

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,15 +35,15 @@ function logout() {
   firebase.auth().signOut();
 }
 
-if (screen.width<1024) {
-	ajustePantallaPequeña();
+if (screen.width < 1024) {
+	  ajustePantallaPequeña();
 } else {
-	ajustePantallaGrande();
+	  ajustePantallaGrande();
 }
 
 
 function ajustePantallaPequeña() {
-	fetch('https://api.edamam.com/search?q='+ randomize() +'&app_id=01dfc015&app_key=ab3ca8c9eb858e5904ba8bc581944e8e&from=0&to=9&calories=gte%20591,%20lte%20722&health=alcohol-free').then(function(response) {
+  fetch('https://api.edamam.com/search?q=' + randomize() + '&app_id=01dfc015&app_key=ab3ca8c9eb858e5904ba8bc581944e8e&from=0&to=9&calories=gte%20591,%20lte%20722&health=alcohol-free').then(function(response) {
 	    return response.json();
 	 })
 	
@@ -51,15 +51,15 @@ function ajustePantallaPequeña() {
     	let cols = 0;
     	let index = 0;
 
-			$.each(data.hits, function(i, food) {
+		  $.each(data.hits, function(i, food) {
 	        index += 1;
-			$(".carousel-inner").append(`<div class="carousel-item img-carousel-${index}"><div class="row justify-content-center"></div>`);
+	  	$('.carousel-inner').append(`<div class="carousel-item img-carousel-${index}"><div class="row justify-content-center"></div>`);
 			 
 
 		    let image = food.recipe.image;
 		    let name = food.recipe.label;
 
-		    $(".img-carousel-"+index + " .row").last().append(`<div class="col-xs-12 recetasRecomendadas">
+		    $('.img-carousel-' + index + ' .row').last().append(`<div class="col-xs-12 recetasRecomendadas">
 				              		<div class="img-thumbnail text-center">
 				                	<a href=""><img class="d-block w-100" src="${image}"></a>
 				                	<div class="carousel-caption d-none d-md-block">
@@ -71,7 +71,7 @@ function ajustePantallaPequeña() {
 }
 
 function ajustePantallaGrande() {
-fetch('https://api.edamam.com/search?q='+ randomize() +'&app_id=01dfc015&app_key=ab3ca8c9eb858e5904ba8bc581944e8e&from=0&to=9&calories=gte%20591,%20lte%20722&health=alcohol-free').then(function(response) {
+  fetch('https://api.edamam.com/search?q=' + randomize() + '&app_id=01dfc015&app_key=ab3ca8c9eb858e5904ba8bc581944e8e&from=0&to=9&calories=gte%20591,%20lte%20722&health=alcohol-free').then(function(response) {
 	    return response.json();
 	 })
 	
@@ -80,30 +80,29 @@ fetch('https://api.edamam.com/search?q='+ randomize() +'&app_id=01dfc015&app_key
     	let index = 0;
 
     	
-			$.each(data.hits, function(i, food) {
-	        if (cols == 0) {
+		  $.each(data.hits, function(i, food) {
+	        if (cols === 0) {
 	        		index += 1;
-			      $(".carousel-inner").append(`<div class="carousel-item img-carousel-${index}"><div class="row justify-content-center"></div>`);
+			      $('.carousel-inner').append(`<div class="carousel-item img-carousel-${index}"><div class="row justify-content-center"></div>`);
 			    }
 		    let image = food.recipe.image;
 		    let name = food.recipe.label;
 
-		    $(".img-carousel-"+index + " .row").last().append(`<div class="col-md-4 recetasRecomendadas">
+		    $('.img-carousel-' + index + ' .row').last().append(`<div class="col-md-4 recetasRecomendadas">
 				              		<div class="img-thumbnail text-center">
 				                	<a href=""><img class="d-block w-100" src="${image}"></a>
 				                	<div class="carousel-caption d-none d-md-block">
 								    	    <h5>${name}</h5>
 								 	        </div></div></div>`);
 
-		    cols +=1;
-    		if (cols == 3) {
+		    cols += 1;
+    		if (cols === 3) {
       			cols = 0;
     		}
     	});
 	
     	
     	$('.img-carousel-1').addClass('active');
-
     });
 }
 
@@ -114,9 +113,9 @@ $('.carousel').carousel();
 function randomize() {
   const lis = ['pollo', 'chocolate', 'carne', 'arroz', 'dulce', 'masa'];
   // while (lis.length) {
-    let result =lis.splice(getRandomInt(0, lis.length), 1)[0];
-    // console.log(result);
-    return result;
+  let result =lis.splice(getRandomInt(0, lis.length), 1)[0];
+  // console.log(result);
+  return result;
   // }
 }
 
@@ -124,37 +123,38 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-/*fetch('https://api.edamam.com/search?q='+ search +'&app_id=01dfc015&app_key=ab3ca8c9eb858e5904ba8bc581944e8e&from=0&to=100&calories=gte%20591,%20lte%20722&health=alcohol-free').then(function(response) {
+/*
+fetch('https://api.edamam.com/search?q='+ search +'&app_id=01dfc015&app_key=ab3ca8c9eb858e5904ba8bc581944e8e&from=0&to=100&calories=gte%20591,%20lte%20722&health=alcohol-free').then(function(response) {
 	    return response.json();
 	 })
 	
     .then(function(data) {
     	console.log(data);
-    });*/
+});*/
   
 
 $('#searchRecetas').on('keypress', function(event) {
   if (event.which === 13) {
-    setSearch($('#searchRecetas').val())
+    setSearch($('#searchRecetas').val());
   }
-})
+});
 
 function setSearch(search) {
-  $('#carrousel').hide()
-	$('#foodList').html("");
-	fetch('https://api.edamam.com/search?q='+ search +'&app_id=01dfc015&app_key=ab3ca8c9eb858e5904ba8bc581944e8e&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free').then(function(response) {
+  $('#carrousel').hide();
+  $('#foodList').html('');
+  fetch('https://api.edamam.com/search?q=' + search + '&app_id=01dfc015&app_key=ab3ca8c9eb858e5904ba8bc581944e8e&from=0&to=100&calories=gte%20591,%20lte%20722&health=alcohol-free').then(function(response) {
 	    return response.json();
-   })
+  })
 	
     .then(function(data) {
     	console.log(data);
     	let index = 0;
       	$.each(data.hits, function(i, food) {
-           index = i + 1;
-           filterElements(data)
-          $("#foodList").append('<div class="row listImg"></div>');
+        index = i + 1;
+        filterElements(data);
+        $('#foodList').append('<div class="row listImg"></div>');
 
-          let recipe = food.recipe
+        let recipe = food.recipe;
         
         $('#foodList .row').append(`<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 imgcont" data-index="${index}">
                                     <div class="card mb-3">
@@ -175,9 +175,9 @@ function setSearch(search) {
                                     </div>
                                     </div>
                                     </div>`);
-		});
-		$('.imgcont').mouseover(getInImg);
-		$('.imgcont').mouseleave(getOutImg);
+		  });
+		  $('.imgcont').mouseover(getInImg);
+		  $('.imgcont').mouseleave(getOutImg);
     });
 }
 
@@ -198,16 +198,16 @@ function diet(recipe) {
 }
 
 function filterElements(data) {
-  $('#foodList').append(`<div class="row"><div class="col-lg-10 col-md-10 col-sm-12 col-12>
-  <input type="text">
-  </div></div>`)
+  if (data.dietLabels.indexOf('Low-Fat') === -1) {
+
+  }
 }
 
 function getInImg() {
   let index = $(this).data('index');
 
-  $('.text-'+index).show();
-  $('.img-'+index).css({'filter': 'brightness(30%)', 
+  $('.text-' + index).show();
+  $('.img-' + index).css({'filter': 'brightness(30%)', 
     '-webkit-filter': 'brightness(30%)',
     '-moz-filter': 'brightness(30%)', 
     '-o-filter': 'brightness(30%)',
@@ -216,14 +216,14 @@ function getInImg() {
     'filter': 'url(grayscale.svg)', // Firefox 4+ 
     'filter': 'gray' // IE 6-9
   });
-  $('.img-'+index).parent().css({'position': 'relative',
+  $('.img-' + index).parent().css({'position': 'relative',
     'z-index': '1', 
     '-webkit-transform': 'scale(1.2)',
     '-moz-transform': 'scale(1.2)',
     '-ms-transform': 'scale(1.2)',
     '-o-transform': 'scale(1.2)',
     'transform': 'scale(1.2)'});
-  $('.text-'+index).css({'position': 'absolute',
+  $('.text-' + index).css({'position': 'absolute',
     'top': '50%',
     'left': '50%',
     'transform': 'translateX(-50%) translateY(-50%)',
@@ -238,18 +238,18 @@ function getInImg() {
 // Al salir de la imagen cambia
 function getOutImg() {
   var index = $(this).data('index');
-  $('.text-'+index).hide();
+  $('.text-' + index).hide();
 
-  $('.img-'+index).parent().css({'position': 'relative',
+  $('.img-' + index).parent().css({'position': 'relative',
     'z-index': '0',
     '-webkit-transition': 'scale(1.0)',
     '-moz-transform': 'scale(1.0)',
     '-ms-transform': 'scale(1.0)',
     '-o-transform': 'scale(1.0)',
     'transform': 'scale(1.0)'});
-  $('.text-'+ index+ 'figure').css({'display': 'none', 
+  $('.text-' + index + 'figure').css({'display': 'none', 
     'background-color': 'transparent'});
-  $('.img-'+index).css({'filter': 'brightness(100%)', 
+  $('.img-' + index).css({'filter': 'brightness(100%)', 
     '-webkit-filter': 'brightness(100%)'});
 }
 
